cffe2a2caaf0277160e5c3ab143559c30185b0b9
natalie <natalie.nd92@gmail.com>
2025-06-13 13:57:31 -0400

    feat(docs): uploading first files

---
+++ CalculatorJava/src/Calculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) { //main is the entrance port to the program
+
+        Scanner entrance = new Scanner(System.in); //Scanner is
+
+
+    }
+}
